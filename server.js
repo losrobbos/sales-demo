8e6701bd7ae6cf825f6a205c178ea5cee0f59342
losrobbos <losrobbos@web.de>
2021-10-22 14:35:52 +0200

    Setup API routes (dummy responses)

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 import express from 'express'
 
+const app = express() // create me an API instance
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from Sales API, D01"})
+})
+
+// ROUTE salespeople
+app.get("/salespeople", (req, res) => {
+  res.send({ message: "Hello, here you will have salespeople sooon" }) // send a response to browser ( => res.write + res.end )
+})
+
+app.get("/topsalesperson", (req, res) => {
+  res.send({ message: "Hello, you get top person here soon, just wait, buddy" })
+})
+
+const PORT = 5000
+app.listen( PORT,  () => {
+  console.log(`API started on http://localhost:${PORT}`)
+})
 
 // API
-
-
 // 2 routes
 
 // Route 1: /salesperson => liest Datei sales.json
 // Route 2: /topsalesperson => liest Datei topsales.json
+
 
